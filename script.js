3771cebe4bbeffa9fce4818e90e8ae1f3b96440d
Juliana Ruane da Silva <julianaruane4@gmail.com>
2025-01-18 21:14:37 -0300

    18-01

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
 
 menuOpenButton.addEventListener("click", () => {
 /*fazendo o menu ser visto*/
 document.body.classList.toggle("show-mobile-menu");
 
 });
+/*fechando menu*/
+menuCloseButton.addEventListener("click", () => menuOpenButton.click())
